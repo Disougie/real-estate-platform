4538c25da7eceb9a3486925783a102879282d56a
disougie <adisougie@gmail.com>
2026-01-28 18:23:37 +0200

    v0

---
+++ src/main/java/com/disougie/RealEstatePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.disougie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealEstatePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealEstatePlatformApplication.class, args);
+	}
+
+}
